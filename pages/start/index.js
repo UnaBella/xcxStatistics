4f13a431f04360992a61fda37c55422cd52b96d7
Una <una_bella@qq.com>
2018-12-29 17:20:17 +0800

    增加登录页
    增加登录页

--- pages/start/index.js
+++ pages/start/index.js
@@ -2,14 +2,67 @@
 const app = getApp();
 Page({
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    num: '',
+    title: ''
   },
-  onLoad: function (options) {
+  userNumInput: function (e) {
+    this.setData({
+      num: e.detail.value
+    })
   },
-  onReady: function () {},
-  onShow: function () {},
-  onUnload: function () {},
+  getUserInfo: function (e) {
+    // console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  bindGetUserInfo: function (e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    }, () => {
+      this.getData()
+    })
 
+    //此处授权得到userInfo
+    // console.log(e.detail.userInfo);
+
+    //接下来写业务代码
+  },
+
+
+  formSubmit: function (e) {
+    // console.log('app.globalData.userInfo，携带数据为：', app.globalData.userInfo)
+
+    // console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    if (!!e.detail.value && !!app.globalData.userInfo.nickName) {
+      this.getData()
+    }
+  },
+  getData: function () {
+    app.Ajax(
+      // 方法组名称为：User（代购用户），不是系统通用用户Users
+      'Users',
+      'POST',
+      'StaffReg',
+      { staffCode: this.data.num, ...app.globalData.userInfo },
+      function (json) {
+        // console.log('json',json);
+        if (json.success) {
+          wx.navigateTo({
+            url: '../index/index',
+          })
+        } else {
+          app.Toast('', 'none', 3000, json.msg.code);
+        }
+      }
+    )
+  },
 
   
 })
