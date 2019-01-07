6622a8f4fc748b95e26d7283deb438079ab3415e
Una <una_bella@qq.com>
2019-01-07 18:09:10 +0800

    增加销售趋势
    增加销售趋势

--- pages/trade/trade.js
+++ pages/trade/trade.js
@@ -1,4 +1,7 @@
 // pages/trade/trade.js
+import F2 from '@antv/wx-f2'; // 注：也可以不引入， initChart 方法已经将 F2 传入，如果需要引入，注意需要安装 @antv/wx-f2 依赖
+
+let chart = null;
 Page({
 
   /**
@@ -21,7 +24,7 @@ Page({
         orderNum: '0',
       },
       index: 0,
-      months: ['2018年9月'],
+      months: ['2019年1月','2018年12月'],
       monthDoc: {
         upOrDown: 0,
         rate: '0',
@@ -29,6 +32,10 @@ Page({
         supplyAmount: '0',
         orderNum: '0'
       }
+    },
+    // 销售趋势
+    salesTrendData: {
+      onInit: salesTrend
     },
   },
 
@@ -46,45 +53,81 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
+
+function salesTrend(canvas, width, height, F2) {
+  const data = [
+    { "month": 1, "type": "销售金额", "value": 490 }, 
+    { "month": 2, "type": "销售金额", "value": 450 }, 
+    { "month": 1, "type": "平台利润", "value": 48 }, 
+    { "month": 2, "type": "平台利润", "value": 43 },
+   ];
+
+  chart = new F2.Chart({
+    el: canvas,
+    width,
+    height
+  });
+
+  chart.source(data, {
+    year: {
+      range: [0, 1],
+      ticks: [1, 2]
+    },
+    value: {
+      tickCount: 10,
+      formatter(val) {
+        return val.toFixed(1);
+      }
+    }
+  });
+
+  chart.tooltip({
+    custom: true, // 自定义 tooltip 内容框
+    onChange(obj) {
+      const legend = chart.get('legendController').legends.top[0];
+      const tooltipItems = obj.items;
+      const legendItems = legend.items;
+      const map = {};
+      legendItems.map(item => {
+        map[item.name] = Object.assign({}, item);
+      });
+      tooltipItems.map(item => {
+        const { name, value } = item;
+        if (map[name]) {
+          map[name].value = value;
+        }
+      });
+      legend.setItems(Object.values(map));
+    },
+    onHide() {
+      const legend = chart.get('legendController').legends.top[0];
+      legend.setItems(chart.getLegendItems().country);
+    }
+  });
+
+  // chart.guide().rect({
+  //   start: [2011, 'max'],
+  //   end: ['max', 'min'],
+  //   style: {
+  //     fill: '#CCD6EC',
+  //     opacity: 0.3
+  //   }
+  // });
+  // chart.guide().text({
+  //   position: [2014, 'max'],
+  //   content: 'Scott administratio\n(2011 to present)',
+  //   style: {
+  //     fontSize: 10,
+  //     textBaseline: 'top'
+  //   }
+  // });
+
+  chart.line().position('month*value').color('type', val => {
+    if (val === '平台利润') {
+      return '#ff0000';
+    }
+  });
+  chart.render();
+  return chart;
+}
